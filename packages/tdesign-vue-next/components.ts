7e192ded0d2f7af44e9a4bb61ba34fc4bb9fa49d
zhangpaopao <zhangpaopao0609@gmail.com>
2024-05-30 17:54:53 +0800

    refactor: components in td

---
+++ packages/tdesign-vue-next/components.ts
@@ -0,0 +1,83 @@
+// 基础
+export * from './src/button';
+export * from './src/icon';
+export * from './src/link';
+
+// 布局
+export * from './src/grid';
+export * from './src/layout';
+export * from './src/divider';
+export * from './src/space';
+
+// 导航
+export * from './src/affix';
+export * from './src/anchor';
+export * from './src/back-top';
+export * from './src/breadcrumb';
+export * from './src/dropdown';
+export * from './src/menu';
+export * from './src/pagination';
+export * from './src/steps';
+export * from './src/sticky-tool';
+export * from './src/tabs';
+
+// 输入
+export * from './src/auto-complete';
+export * from './src/cascader';
+export * from './src/checkbox';
+export * from './src/color-picker';
+export * from './src/date-picker';
+export * from './src/form';
+export * from './src/input-adornment';
+export * from './src/input';
+export * from './src/range-input';
+export * from './src/input-number';
+export * from './src/radio';
+export * from './src/select';
+export * from './src/slider';
+export * from './src/switch';
+export * from './src/tag-input';
+export * from './src/select-input';
+export * from './src/textarea';
+export * from './src/transfer';
+export * from './src/time-picker';
+export * from './src/tree-select';
+
+// 数据展示
+export * from './src/avatar';
+export * from './src/badge';
+export * from './src/calendar';
+export * from './src/card';
+export * from './src/comment';
+export * from './src/descriptions';
+export * from './src/image';
+export * from './src/image-viewer';
+export * from './src/list';
+export * from './src/loading';
+export * from './src/progress';
+export * from './src/skeleton';
+export * from './src/statistic';
+export * from './src/swiper';
+export * from './src/table';
+export * from './src/tag';
+export * from './src/tooltip';
+export * from './src/tree';
+export * from './src/watermark';
+export * from './src/collapse';
+export * from './src/rate';
+export * from './src/timeline';
+
+// 消息提醒
+
+export * from './src/alert';
+export * from './src/dialog';
+export * from './src/drawer';
+export * from './src/guide';
+export * from './src/message';
+export * from './src/notification';
+export * from './src/popconfirm';
+export * from './src/popup';
+export * from './src/upload';
+
+// 全局配置
+export * from './src/config-provider';
